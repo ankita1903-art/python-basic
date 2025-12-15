79c5e59f0d581fe8fed9caf7d6cb17a3e91f206f
ANKITA ROY <ankita.roy.24@aot.edu.in>
2025-12-15 12:33:41 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,12 @@
+#personal intro
+a='Hello,i am Ankita Roy,persuing my B.Tech degree from Academy of Technology.'
+b='I live in Brrackpore.'
+c='I have completed my 10 and 10+2 education from Ramakrishna Vivekananda Mission Vidyabhawan.'
+print(a,b,c)
+#Project: Personal Introduction Program
+a=input("What is your name?")
+b=input("How old are you?")
+c=input("Where you from?")
+d=input("What is your favorite food?")
+print("Welcom",a)
+print("You are",b,"years old,from",c,"and your favorite food is",d)
